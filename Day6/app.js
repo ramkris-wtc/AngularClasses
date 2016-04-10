42230e54016a33172f4bed871afb790b8318b2d8
RamKris <ramakrishna.t@walkingtree.in>
2016-04-10 06:37:42 +0000

    more http stuff; header config, interceptors, transformations, ngresource

--- Day6/app.js
+++ Day6/app.js
@@ -1,7 +1,7 @@
 // angular.module('myApp', []);
 
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngResource']);
 
 
 app.controller('TestController', function($q, $timeout){
@@ -92,23 +92,103 @@ app.controller('BookController', function(BookService) {
 
 })
 
-app.service('BookService', function($http){
-	this.getBooks = function() {
-		return $http.get('http://localhost:3000/books').
-		then(function(response){
-			return response.data;
-		}, function(error){
-			console.log(error)
-		})
+app.service('BookService', function($resource){
+	var Book = $resource('https://angularclasses-ramkriswtc.c9users.io:8081/books');
+
+	this.getBooks = function () {
+		return Book.query().$promise
 	}
 
-	this.addBook = function(book) {
-		return $http.post('http://localhost:3000/books', book).
-		then(function(response){
-			return response.data;
-		}, function(error){
-			console.log(error)
-		})
+	this.addBook = function (book) {
+		// body...
+		return Book.save(book).$promise;
 	}
+})
+
+// app.service('BookService', function($http){
+// 	this.getBooks = function() {
+// 		return $http.get('https://angularclasses-ramkriswtc.c9users.io:8081/books', {
+// 			transformResponse: function(data){
+// 				var books = [];
+// 				console.log(data);
+// 				data = JSON.parse(data)
+// 				data.forEach(function(book){
+// 					books.push({
+// 						"id": book.id,
+// 						"title": book.title,
+// 						"author": book.author
+// 					})
+// 				})
+// 				console.log(books);
+// 				return books;
+// 				//return JSON.parse(data);
+// 			}
+// 		}).
+// 		then(function(response){
+// 			return response.data;
+// 		}, function(error){
+// 			console.log(error)
+// 		})
+// 	}
+
+// 	// this.addBook = function(book) {
+// 	// 	return $http.post('https://angularclasses-ramkriswtc.c9users.io:8081/books', book).
+// 	// 	then(function(response){
+// 	// 		return response.data;
+// 	// 	}, function(error){
+// 	// 		console.log(error)
+// 	// 	})
+// 	// }
+
+// 	this.addBook = function(book) {
+// 		return $http.post('http://localhost:3000/books', book, {
+// 			headers: {
+// 				'MyHeader':'XXXXXXXXXX'
+// 			}
+// 		}).
+// 		then(function(response){
+// 			return response.data;
+// 		}, function(error){
+// 			console.log(error)
+// 		})
+// 	}
+// })
+
+app.service('UserService', function(){
+	this.currentUser = {
+		name: 'John',
+		email: 'xxx@yyyy.com',
+		auth_token: 'dshjak838shsfj+sj2!33'
+	}
+})
+
+app.factory('apiInterceptor', function(UserService){
+	return {
+		request: function(config) {
+			if(UserService.currentUser){
+				config.headers['auth_token'] = UserService.currentUser.auth_token;
+				console.log(config)
+			}
+			return config;
+		},
+
+		requestError: function(config) {
+			return config;
+		},
+
+		response: function(res) {
+			return res;
+		},
+
+		responseError: function(res) {
+			return res;
+		}
+	}
+})
+
+app.config(function($httpProvider){
+	//$httpProvider.defaults.headers.common["MyHeader1"] = 'ZZZZZZZZZZZ';
+	$httpProvider.defaults.headers.post["MyHeader1"] = 'ZZZZZZZZZZZ';
+	$httpProvider.interceptors.push('apiInterceptor');
 })
 
